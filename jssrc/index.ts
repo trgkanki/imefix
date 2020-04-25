b1471d6397d9004bb68258d87058f5722af5c174
Hyun Woo Park <phu54321@naver.com>
2020-04-25 16:16:55 +0900

    :sparkles: no ime repeat on ctrl+enter

--- jssrc/index.ts
+++ jssrc/index.ts
@@ -1,1 +1,21 @@
-alert('Test addon initiailized')
+const hangulCharacterRegex = /^[\u3131-\uD79D]$/
+
+document.addEventListener('compositionend', (_e) => {
+  const e = _e as CompositionEvent
+  const target = e.target as (HTMLElement | null)
+  if (target) {
+    target.dataset.lastCompositionEndString = e.data
+  }
+}, { capture: true })
+
+document.addEventListener('keydown', (e) => {
+  const target = e.target as (HTMLElement | null)
+  if (
+    target &&
+    !e.isComposing &&
+    e.key === target.dataset.lastCompositionEndString &&
+    hangulCharacterRegex.test(e.key)
+  ) {
+    e.preventDefault()
+  }
+}, { capture: true })
